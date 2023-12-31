2424343a3e16081ae54fa5eac98226f2371bba15
Pankaj Yadav <ypankaj651@gmail.com>
2023-12-31 19:44:07 +0530

    added discord functionality with caching and logging

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import requests
 import os
 import json
+import sys
+import logging
 from datetime import datetime, date
 from urllib.parse import quote
 from dotenv import load_dotenv
+from discord_webhook import DiscordWebhook
 
 load_dotenv()   # initializing env vars from .env file
 api = os.environ.get('api_key')
+discord_url = os.environ.get('discord_url')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def send_discord(discord_url, message):
+    webhook = DiscordWebhook(url=discord_url, content=message)
+    response = webhook.execute()
+    if response.status_code == 200:
+        logging.info("Successfully sent to discord.")
+    else:
+        logging.error("Failed to send to Discord.")
 
 def get_inr_to_usd_rate():
     try:
@@ -14,7 +27,8 @@ def get_inr_to_usd_rate():
             file_data = f.readline().split(':')
             if datetime.strptime(file_data[0], "%Y-%m-%d").date() >= date.today():
                 return float(file_data[1])
-    except:
+    except Exception as e:
+        logging.error(str(e))
         return None
 
 def cache_inr_to_usd_rate(rate):
@@ -34,10 +48,37 @@ def inr_to_usd(amount):
 def round_float(num):
     return round(float(num), 4)
 
+def prettify(data):
+    result = ""
+    keys = list(data.keys())
+    keys.remove('id')
+    for key in keys:
+        result += "{}\n".format(data[key])
+    return result
+
+def cache_item(item):
+    with open('data','a') as f:
+        f.write(json.dumps(item))
+        f.write('\n')
+    return 0
+
+def check_cache(item):
+    try:
+        with open('data', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if json.loads(line)['id'] == item['id']:
+                    return 1
+    except Exception as e:
+        logging.error(str(e))
+        cache_item(item=item)
+        return 0
+
 def csmoney_parser(raw_data):
     items = []
     for item in raw_data['items']:
         data = {}
+        data['id'] = item['id']
         data['name'] = item['asset']['names']['full']
         data['float'] = round_float(item['asset']['float'])
         data['price'] = "{} INR".format(int(item['pricing']['computed']/inr_to_usd(1)))
@@ -52,14 +93,21 @@ def csmoney_parser(raw_data):
     return items
 
 def csmoney_scraper(name, max_float, max_price, wear ):
-    url = "https://cs.money/1.0/market/sell-orders?limit=60&maxFloat={}&maxPrice={}&name={}&quality={}".format(max_float, max_price, quote(name), wear)
+    url = "https://cs.money/1.0/market/sell-orders?limit=60&maxFloat={}&maxPrice={}&name={}".format(max_float, max_price, quote(name))
+    for quality in wear:
+        url += "&quality={}".format(quality)
     response = requests.get(url=url)
     if response.status_code == 200:
         raw_data = json.loads(response.content)
         return csmoney_parser(raw_data=raw_data)
     else:
-        print(response.status_code)
+        logging.error(response.status_code)
         return
 
-for item in csmoney_scraper(name="AK-47 Bloodsport", max_float=0.2, max_price=inr_to_usd(amount=7000), wear="ft"):
-    print(item)
+def main():
+    for item in csmoney_scraper(name="AK-47 Bloodsport", max_float=0.2, max_price=inr_to_usd(amount=7200), wear=["ft", "mw"]):
+        if not check_cache(item=item):
+            cache_item(item=item)
+            send_discord(discord_url=discord_url, message=prettify(item))
+
+main()
