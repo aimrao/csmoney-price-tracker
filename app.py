afdfdca9d0f8d147571c1ee2d8dcd4ed3897fb5d
Pankaj Yadav <ypankaj651@gmail.com>
2024-01-02 00:58:20 +0530

    cleaned code and added comments/docstrings

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from main import main
 from time import sleep
 
+# call the main function from main.py and run it every 10 minutes
 while True:
     main()
     sleep(600)
--- main.py
+++ main.py
@@ -8,14 +8,19 @@ from urllib.parse import quote
 from dotenv import load_dotenv
 from discord_webhook import DiscordWebhook
 
-load_dotenv()   # initializing env vars from .env file
+load_dotenv()   # load env file
+
+# initializing env vars from .env file
 api = os.environ.get('api_key')
 discord_url = os.environ.get('discord_url')
 data_dir = os.environ.get('data_dir')
 queries = os.environ.get('queries')
+
+# set logging level and format
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def send_discord(discord_url, message):
+    '''Send message to discord webhook'''
     webhook = DiscordWebhook(url=discord_url, content=message)
     response = webhook.execute()
     if response.status_code == 200:
@@ -24,8 +29,9 @@ def send_discord(discord_url, message):
         logging.error("Failed to send to Discord.")
 
 def get_inr_to_usd_rate():
+    '''Read and return the INR->USD conversion rate from file'''
     try:
-        with open('inr_to_usd_rate','r') as f:
+        with open('{}/inr_to_usd_rate'.format(data_dir),'r') as f:
             file_data = f.readline().split(':')
             if datetime.strptime(file_data[0], "%Y-%m-%d").date() >= date.today():
                 return float(file_data[1])
@@ -34,11 +40,13 @@ def get_inr_to_usd_rate():
         return None
 
 def cache_inr_to_usd_rate(rate):
-    with open('inr_to_usd_rate','w') as f:
+    '''Cache the INR->USD conversion rate to file with ttl of a day'''
+    with open('{}/inr_to_usd_rate'.format(data_dir),'w') as f:
         f.write("{}:{}".format(date.today(),rate))
     return 1
 
 def inr_to_usd(amount):
+    '''Get INR->USD conversion rate from fixer.io and cache it if not available in file or stale'''
     if get_inr_to_usd_rate() == None:
         response = json.loads(requests.get("http://data.fixer.io/api/latest?access_key={}&symbols=USD,INR".format(api, amount)).content)
         inr_to_usd_rate = response['rates']['USD']/response['rates']['INR']
@@ -48,9 +56,11 @@ def inr_to_usd(amount):
     return round_float(inr_to_usd_rate*amount)
 
 def round_float(num):
+    '''Round the float value to 4 digits'''
     return round(float(num), 4)
 
 def prettify(data):
+    '''Prettify the input dictionary'''
     result = ""
     keys = list(data.keys())
     keys.remove('id')
@@ -59,12 +69,14 @@ def prettify(data):
     return result
 
 def cache_item(item):
+    '''Cache the scraped item to file'''
     with open('{}/data'.format(data_dir),'a') as f:
         f.write(json.dumps(item))
         f.write('\n')
     return 0
 
 def check_cache(item):
+    '''Check whether item is already present in the data file i.e. if it has been already scraped and sent to discord.'''
     try:
         with open('{}/data'.format(data_dir), 'r') as f:
             lines = f.readlines()
@@ -77,6 +89,7 @@ def check_cache(item):
         return 0
 
 def csmoney_parser(raw_data):
+    '''Parse the item list response from csmoney'''
     items = []
     for item in raw_data['items']:
         data = {}
@@ -95,6 +108,7 @@ def csmoney_parser(raw_data):
     return items
 
 def csmoney_scraper(name, max_float, max_price, wear ):
+    '''Get item list from csmoney endpoint'''
     url = "https://cs.money/1.0/market/sell-orders?limit=60&maxFloat={}&maxPrice={}&name={}".format(max_float, max_price, quote(name))
     for quality in wear:
         url += "&quality={}".format(quality)
@@ -107,6 +121,7 @@ def csmoney_scraper(name, max_float, max_price, wear ):
         return []
 
 def main():
+    '''Run the csmoney scraper function for each query in env file'''
     for query in json.loads(queries):
         for item in csmoney_scraper(name=query[0], max_float=query[1], max_price=inr_to_usd(amount=query[2]), wear=query[3]):
             if not check_cache(item=item):
